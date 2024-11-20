770fdeebc48a0914ec19719f53b225a7d826deb5
NateKenopic <nkenopic@outlook.com>
2024-11-19 20:13:03 -0500

    Updated the Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,10 +6,17 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                <div class="grid gap-6 lg:grid-cols-[30%_minmax(0,_1fr)] lg:gap-8">
+                    <div class="p-6 text-gray-900 overflow-hidden m-3 bg-gray-700 dark:text-gray-100">
+                        <div>
+                            <livewire:book.book-modal />
+                        </div>
+                    </div>
+                    <div class="p-6 text-gray-900 overflow-hidden m-3 bg-gray-700 dark:text-gray-100">
+                        {{ __("You're logged in!") }}
+                    </div>
                 </div>
             </div>
         </div>
